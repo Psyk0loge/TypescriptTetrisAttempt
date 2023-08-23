22c9f3f05aa76a9fa1429e61034342a13b6179ef
Psyk0loge <SimonGoetz99@gmx.de>
2023-08-23 22:12:46 +0200

    irgendwie funktioniert meine Steuerung nicht...

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,71 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-const playerBlock = document.querySelector<HTMLDivElement>("#color-box")
+const playerBlock = <HTMLDivElement>document.getElementById("ballID");
 
-if(playerBlock != null){
-
-  console.log(playerBlock.offsetTop)
-  for(let i = 0; i<100; i++){
-    playerBlock.style.top +=1
-  }
-
-  
+const directions = {
+  Left : 'left',
+  Right : 'right',
+  Up : 'up',
+  Down : 'down',
 }
 
-console.log("HI")
+window.addEventListener("keydown",
+  x =>{
+    playerControl(x.key)
+  }
+  
+
+)
+
+let margin = 0;
+
+let w = 980
+let l = window.screen.width
+//let myVar = setInterval(moveItems,10)
+function moveItems(direction : string){
+  console.log(playerBlock)
+  if(playerBlock != null && playerBlock != undefined){
+    console.log(w)
+    if(margin >= w){
+      margin = 0
+    }else{
+      playerBlock.style.marginLeft = margin + "px";
+    }
+    margin += 3
+  }
+}
+
+function playerControl(key : string){
+  const playerBlock = <HTMLDivElement>document.getElementById("ballID");
+  if(playerBlock != null && playerBlock != undefined){
+    switch(key) {
+      case "ArrowUp":
+        var top = parseInt(playerBlock?.style.marginTop) + 10
+        playerBlock.style.marginTop = top + "px";
+        console.log("Up")
+        moveItems(directions.Up)
+        break
+      case "ArrowDown":  
+        var top = parseInt(playerBlock?.style.marginTop) - 10
+        playerBlock.style.marginTop = top + "px";
+        console.log("Down")
+        break
+      case "ArrowLeft":  
+        var top = parseInt(playerBlock?.style.left) - 10
+        playerBlock.style.left = top + "px";
+        console.log("Left")
+        break
+      case "ArrowRight":  
+        var top = parseInt(playerBlock?.style.left) + 10
+        playerBlock.style.left = top + "px";
+        console.log("Right")
+        break
+      }
+  }
+}
+
+
+function getPlayer(elementClass : string): HTMLDivElement{
+  return <HTMLDivElement>document.getElementsByClassName(elementClass)[0]
+}
